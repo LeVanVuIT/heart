b1947bc6299facca4abe03b5b409a94d15a6555c
Lê Văn Vũ <levanvu@bacoor.co>
2022-11-09 11:59:28 +0700

    udpate component

---
+++ src/Screen/Components/Heart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+`
+const HeartScreen = () =>{
+    return(
+        <Container>
+            <button>
+                Hi!
+            </button>
+        </Container>
+    )
+}
+export default HeartScreen
