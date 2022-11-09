35925c0e52252bbf6d8102a584071ee1a53ac7a3
Lê Văn Vũ <levanvu@bacoor.co>
2022-11-09 13:34:38 +0700

    udpate

--- src/Screen/Components/Heart.js
+++ src/Screen/Components/Heart.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { Button } from 'antd'
+import images from '../../config/images'
 
 const Container = styled.div`
 `
+const Action = styled(Button)`
+`
 const HeartScreen = () =>{
     return(
-        <Container>
-            <button>
-                Hi!
-            </button>
+        <Container className="wrapper">
+            <Action>
+                Hiiiiiii!
+            </Action>
         </Container>
     )
 }
