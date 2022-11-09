6e57b89b5d66db2a2c6ed788e1037ce3b6a46972
Lê Văn Vũ <levanvu@bacoor.co>
2022-11-09 13:59:52 +0700

    udpate UI

--- src/Screen/Components/Heart.js
+++ src/Screen/Components/Heart.js
@@ -6,12 +6,26 @@ import images from '../../config/images'
 const Container = styled.div`
 `
 const Action = styled(Button)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-image: linear-gradient(to right, #ff4b1f 0%, #1fddff  51%, #ff4b1f  100%);   
+    padding: 15px 45px;
+    text-align: center;
+    transition: 0.5s;
+    background-size: 200% auto;
+    color: white;            
+    box-shadow: 10px 5px 10px 0px #ff4b1f;
+    border-radius: 10px; 
+    border: none;
+
 `
 const HeartScreen = () =>{
     return(
         <Container className="wrapper">
             <Action>
-                Hiiiiiii!
+                xxx
             </Action>
         </Container>
     )
